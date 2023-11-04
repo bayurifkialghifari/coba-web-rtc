687241df442a6481928df04ff11713f23fb08006
bayurifkialghifari <bayurifkialgh@gmail.com>
2023-11-04 20:44:45 +0700

    fix peer js and websocket

--- src/handler/websocket/test.js
+++ src/handler/websocket/test.js
@@ -13,6 +13,11 @@ class TestHandler {
       // Join the room
       client.join(roomId)
       client.to(roomId).emit('user-connected', userId)
+
+      // Client disconnected
+      client.on('disconnect', () => {
+        client.to(roomId).emit('user-disconnected', userId)
+      })
     })
   }
 }
